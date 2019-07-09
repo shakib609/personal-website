67aea4711688272e67ae5472756468278ffc9ef1
Shakib Hossain <shakib609@gmail.com>
2019-07-09 19:10:48 +0600

    Update website config

--- config/website.js
+++ config/website.js
@@ -1,28 +1,29 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
   siteTitle: 'shakib609 Blog', // Navigation and Site Title
-  siteTitleAlt: 'The blog of Shakib Hossain(shakib609)', // Alternative Site title for SEO
+  siteTitleAlt: 'Personal blog of Shakib Hossain', // Alternative Site title for SEO
   siteTitleShort: 'shakib609 blog', // short_name for manifest
   siteUrl: 'https://blog.shakib609.gq', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'Personal blog of Shakib Hossain',
+  siteDescription:
+    'Personal blog of Shakib Hossain. I write here about my journey of Python, Django, JavaScript, React, Flutter etc.',
   author: 'Shakib Hossain', // Author for schemaORGJSONLD
-  organization: 'shakib609',
+  organization: '',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@eggheadio', // Twitter Username
-  ogSiteName: 'egghead.io Gatsby Starter Blog', // Facebook Site Name
+  userTwitter: '@shakib609', // Twitter Username
+  ogSiteName: "Shakib Hossain's Personal Blog", // Facebook Site Name
   ogLanguage: 'en_US',
-  googleAnalyticsID: '',
+  googleAnalyticsID: 'UA-78902575-1',
 
   // Manifest and Progress color
   themeColor: '#5348FF',
   backgroundColor: '#2b2e3c',
 
   // Social component
-  twitter: 'https://twitter.com/eggheadio/',
-  twitterHandle: '@eggheadio',
-  github: 'https://github.com/eggheadio/',
-  linkedin: '',
+  twitter: 'https://twitter.com/shakib609/',
+  twitterHandle: '@shakib609',
+  github: 'https://github.com/shakib609/',
+  linkedin: 'https://www.linkedin.com/in/shakib609/',
 }
